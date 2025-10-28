9f57068d227335ad4ff681c56ad8198bd26c2b31
USER <tamimachy9897@gmail.com>
2025-10-28 21:26:51 +0600

    update product class

---
+++ Models/Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mini_Inventory_System.Models.Domain
+{
+    [Index(nameof(Barcode), IsUnique = true)]
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public Decimal Price { get; set; }
+        public Decimal StockQty { get; set; }
+        public string Category { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Learn more about configuring Swagger at https://learn.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-8.0&tabs=visual-studio
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +17,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
