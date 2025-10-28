780915b662bcf60546d0972e9f0e1ac4a5da5924
USER <tamimachy9897@gmail.com>
2025-10-28 22:04:45 +0600

    All classes are created!

--- Models/Domain/Customer.cs
+++ Models/Domain/Customer.cs
@@ -2,15 +2,12 @@
 
 namespace Mini_Inventory_System.Models.Domain
 {
-    [Index(nameof(Barcode), IsUnique = true)]
     public class Customer
     {
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public string Barcode { get; set; }
-        public Decimal Price { get; set; }
-        public Decimal StockQty { get; set; }
-        public string Category { get; set; }
-        public bool Status { get; set; }
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int LoyaltyPoints { get; set; }
     }
 }
---
+++ Models/Domain/Sale.cs
@@ -0,0 +1,13 @@
+﻿namespace Mini_Inventory_System.Models.Domain
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Customer CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal DueAmount { get; set; }
+        List<Product> SaleDetails { get; set; }
+    }
+}
